3f8cb02c5ae48cd735987364d6043ce75716503a
WilliamCappelletti <cappelletti.william@gmail.com>
2021-04-20 11:04:03 +0200

    Include only relevant for notebook

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,13 @@
 __all__ = [
-    "explain",
-    "metrics",
-    "utility",
-    "visualization"
+    "plot_mol",
+    "compute_accuracy",
+    "compute_cm",
+    "plot_cm",
+    "GNNExplainer",
+    "make_interactive_explainer",
 ]
 
-from . import explain, metrics, utility, visualization
+from .explain import GNNExplainer
+from .metrics import compute_accuracy, compute_cm, plot_cm
+from .utility import make_interactive_explainer
+from .visualization import plot_mol
